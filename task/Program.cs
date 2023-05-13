f65055871df0b2be8315eed21de21516a57e0f7c
zhannas <zhannas@gmail.com>
2023-05-13 20:11:27 +0200

    task

---
+++ task/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Enter array size: ");
+int size = ReadTextFromConsole();
+string[] arrayText = new string[size];
+
+int ReadTextFromConsole()
+{
+    string input = Console.ReadLine()!;
+    int result = int.Parse(input);
+    return result;
+}
+
+void FillArrayConsoleWords(string[] arrayText)
+{
+    for(int i = 0; i < arrayText.Length; i++)
+    {
+        Console.WriteLine($"Enter {i+1} array word");
+        arrayText[i] = Console.ReadLine()!;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+        {
+          Console.Write($"{array[i]} ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+string [] text(string[] arrayText)
+{
+    int count = 0;
+    for (int i = 0; i < arrayText.Length; i++)
+    {
+      if(arrayText[i].Length <= 3)
+      count++;
+    }
+    string [] result = new string [size];
+    int j = 0;
+    for (int i = 0; i < arrayText.Length; i++)
+    {
+      if(arrayText[i].Length <= 3)
+      {
+        result[j] = arrayText[i];
+        j++;
+      }
+    }
+    return result;
+}
+        
+FillArrayConsoleWords(arrayText);
+PrintArray(arrayText);
+PrintArray(text(arrayText));   
+
+
